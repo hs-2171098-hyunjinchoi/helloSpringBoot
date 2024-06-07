0793d24bf8f6c755c93addca47c1c7ce0634fb1d
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-06-07 16:51:35 +0900

    test: Application Properties
    - @Value 사용하는 방법

--- src/main/java/kr/ac/hansung/cse/hellospringboot/controller/HelloWorldController.java
+++ src/main/java/kr/ac/hansung/cse/hellospringboot/controller/HelloWorldController.java
@@ -1,15 +1,28 @@
 package kr.ac.hansung.cse.hellospringboot.controller;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloWorldController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Value("${app.professor}")
+    private String professor;
+
+    @Value("${app.course}")
+    private String course;
 
     @GetMapping("/")
     public String sayHello(Model model) {
+
+        logger.debug("proffesor name: {}", professor);
+        logger.debug("course name: {}", course);
+
         model.addAttribute("msg", "Hello World");
         return "index";
     }
