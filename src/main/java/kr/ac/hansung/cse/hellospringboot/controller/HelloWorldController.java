ee6044ec13675ede75cc45b9f0c003349ea0d0d7
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-06-07 17:02:56 +0900

    test: Application Properties
    - ConfigurationProperties 사용

--- src/main/java/kr/ac/hansung/cse/hellospringboot/controller/HelloWorldController.java
+++ src/main/java/kr/ac/hansung/cse/hellospringboot/controller/HelloWorldController.java
@@ -1,7 +1,9 @@
 package kr.ac.hansung.cse.hellospringboot.controller;
 
+import kr.ac.hansung.cse.hellospringboot.configuration.MyConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,19 +13,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloWorldController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Value("${app.professor}")
-    private String professor;
+//    @Value("${app.professor}")
+//    private String professor;
+//
+//    @Value("${app.course}")
+//    private String course;
 
-    @Value("${app.course}")
-    private String course;
+    @Autowired
+    MyConfiguration myConfiguration;
 
     @GetMapping("/")
     public String sayHello(Model model) {
 
-        logger.debug("proffesor name: {}", professor);
-        logger.debug("course name: {}", course);
+//        logger.debug("professor name: {}", professor);
+//        logger.debug("course name: {}", course);
+
+        logger.debug("professor name: {}", myConfiguration.getProfessor());
+        logger.debug("course name: {}", myConfiguration.getCourse());
 
         model.addAttribute("msg", "Hello World");
+        model.addAttribute("appConfig", myConfiguration);
         return "index";
     }
 }
