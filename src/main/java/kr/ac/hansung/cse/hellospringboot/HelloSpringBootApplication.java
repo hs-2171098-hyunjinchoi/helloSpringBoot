aeea97ef04b9fb348dd64e879d26f837b1df5af6
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-06-07 15:33:26 +0900

    test: actuator, configuration

---
+++ src/main/java/kr/ac/hansung/cse/hellospringboot/HelloSpringBootApplication.java
@@ -0,0 +1,24 @@
+package kr.ac.hansung.cse.hellospringboot;
+
+import kr.ac.hansung.cse.hellospringboot.service.MyService;
+
+import org.springframework.boot.SpringApplication;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class HelloSpringBootApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                 SpringApplication.run(HelloSpringBootApplication.class, args);
+
+        MyService myService = context.getBean(MyService.class);
+
+        System.out.println(myService.getMessage());
+    }
+
+}
